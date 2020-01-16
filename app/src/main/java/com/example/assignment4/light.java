7ba9cf00832aae4ede120e11066ea2894de706d9
Vaibhav <vaibhavatara@example.com>
2020-01-16 19:08:13 +0530

    dynamic change in color

--- app/src/main/java/com/example/assignment4/light.java
+++ app/src/main/java/com/example/assignment4/light.java
@@ -3,11 +3,14 @@ package com.example.assignment4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.text.Layout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class light extends AppCompatActivity implements SensorEventListener {
@@ -15,11 +18,13 @@ public class light extends AppCompatActivity implements SensorEventListener {
     SensorManager sensorManager;
     Sensor light;
     private TextView lightvalue;
+    LinearLayout light_layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
         lightvalue = (TextView)findViewById(R.id.lightvalue);
+        light_layout = (LinearLayout)findViewById(R.id.light_layout);
 
 
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
@@ -39,6 +44,21 @@ public class light extends AppCompatActivity implements SensorEventListener {
     public void onSensorChanged(SensorEvent sensorEvent) {
 
         lightvalue.setText(""+(sensorEvent.values[0]));
+
+        if(sensorEvent.values[0]>0 && sensorEvent.values[0]<50)
+            light_layout.setBackgroundColor(Color.parseColor("#7955d4"));
+
+        if(sensorEvent.values[0]>49 && sensorEvent.values[0]<100)
+            light_layout.setBackgroundColor(Color.parseColor("#552fb5"));
+
+        if(sensorEvent.values[0]>99 && sensorEvent.values[0]<150)
+            light_layout.setBackgroundColor(Color.parseColor("#411aa3"));
+
+        if(sensorEvent.values[0]>149 && sensorEvent.values[0]<200)
+            light_layout.setBackgroundColor(Color.parseColor("#3108dc"));
+
+        if(sensorEvent.values[0]>199)
+            light_layout.setBackgroundResource(R.drawable.custom_background);
 
 
     }
--- app/src/main/java/com/example/assignment4/proximity.java
+++ app/src/main/java/com/example/assignment4/proximity.java
@@ -3,11 +3,13 @@ package com.example.assignment4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class proximity extends AppCompatActivity implements SensorEventListener {
@@ -15,12 +17,13 @@ public class proximity extends AppCompatActivity implements SensorEventListener 
     SensorManager sensorManager;
     Sensor Proximity;
     private TextView proximityvalue;
+    LinearLayout proximity_layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proximity);
         proximityvalue = (TextView)findViewById(R.id.proximityvalue);
-
+        proximity_layout = (LinearLayout)findViewById(R.id.proximity_layout);
 
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         Proximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
@@ -39,6 +42,16 @@ public class proximity extends AppCompatActivity implements SensorEventListener 
     public void onSensorChanged(SensorEvent sensorEvent) {
 
         proximityvalue.setText(""+(sensorEvent.values[0]));
+        if(sensorEvent.values[0] == 0)
+        {
+            proximity_layout.setBackgroundResource(R.drawable.custom_background2);
+        }
+        else
+        {
+            proximity_layout.setBackgroundResource(R.drawable.custom_background);
+
+        }
+
 
     }
 
